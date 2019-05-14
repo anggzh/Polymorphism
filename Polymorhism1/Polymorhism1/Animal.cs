111e74ee6514cfc423786a813b1ba338abb0cd68
anggzh <45726270+anggzh@users.noreply.github.com>
2019-05-14 14:41:09 +0700

    Add files via upload

---
+++ Polymorhism1/Polymorhism1/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorhism1
+{
+    class Animal
+    {
+        public string Name { get; set; }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("The Animal speaks");
+        }
+
+        public class Cat : Animal
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("Cat says: Meong-meong");
+            }
+
+        }
+
+        public class Dog : Animal
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("Dog says: Guk-guk");
+            }
+
+}
+
+        public class Chicken : Animal
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("Chicken says: Kukuruyuk");
+            }
+        }
+    }
+}
---
+++ Polymorhism1/Polymorhism1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorhism1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Speak();
+
+            Animal.Cat cat = new Animal.Cat();
+            animal = cat;
+            animal.Speak();
+
+            Animal.Chicken chicken = new Animal.Chicken();
+            animal = chicken;
+            animal.Speak();
+
+            Animal.Dog dog = new Animal.Dog();
+            animal = dog;
+            animal.Speak();
+
+            Console.ReadKey();
+        }
+    }
+}
